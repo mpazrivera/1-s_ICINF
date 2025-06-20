e4a23f6df308c63b2f3d66e70fabbf103e241305
mpazrivera <mariapaz.rivera@alumnos.ulagos.cl>
2025-06-20 00:35:56 -0400

    Add files via upload

--- apuntes/03.listas.py
+++ apuntes/03.listas.py
@@ -17,8 +17,15 @@ estudiantes = ["vale", "pachi", "messi", "vale"]
 print(estudiantes[0])
 '''
 Esta función es count( ). Queremos saber la cantidad de ocurrencias del elemento
-“Marco” en la lista estudiantes, en este caso el resultado que debe arrojarnos
+“Vale” en la lista estudiantes, en este caso el resultado que debe arrojarnos
 la terminal sería 2 ocurrencias
 '''
 print(estudiantes. count ("vale"))
 
+#Funcion para unir dos listas, tuplas,ect.
+jugadores_euro = ["messi", "ronaldo", "modric", "kross", "pedri", "lewandowski"]
+jugadores_chilenos = ["paredes", "fierro","vidal","sanchez", "pizarro","bravo"]
+print(jugadores_euro)
+print(jugadores_chilenos)
+jugadores = list(zip(jugadores_euro,jugadores_chilenos))
+print(jugadores)
