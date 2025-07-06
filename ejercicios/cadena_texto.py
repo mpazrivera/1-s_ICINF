1f045d1217c03dfb193451e9bc6f3847166ad26a
mpazrivera <mariapaz.rivera@alumnos.ulagos.cl>
2025-07-06 17:00:48 -0400

    Add files via upload

--- ejercicios/cadena_texto.py
+++ ejercicios/cadena_texto.py
@@ -1,4 +1,19 @@
+#ingresar una frase de 30 caracteres incluyendo los espacios.
+frase = input("Ingrese una frase (máximo 30 caracteres): ")
+len(frase) <= 30
 
-# A. Ingresar una frase de máximo 30 caracteres
-frase = str(input("Ingresa una frase de máximo 30 caracteres: "))[ :30]
-print(contar_a(frase))
+#Crear 2 variables, una para la minusculas y otra para las mayusculas
+frase_mayuscula = frase.upper()
+frase_minuscula = frase.lower()
+
+#Contar cuantas veces esta la a
+contar_a = frase_minuscula.count("a")
+
+#Imprimir la longitud de la frase
+longitud = len(frase)
+
+# Mostrar resultados
+print("\nFrase en mayúsculas:", frase_mayuscula)
+print("Frase en minúsculas:", frase_minuscula)
+print("Cantidad de letras 'a' o 'A':", contar_a)
+print("Longitud de la frase:", longitud)
